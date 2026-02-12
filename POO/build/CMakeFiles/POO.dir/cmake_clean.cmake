57d9a2f6a8552c0e1b0009d9ac568208676116c7
Rafael Fariñas <rafael.farinas@mi.unc.edu.ar>
2026-02-12 15:54:18 -0300

    Compilamos el codigo

---
+++ POO/build/CMakeFiles/POO.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POO.dir/link.d"
+  "CMakeFiles/POO.dir/src/Tiempo.cpp.o"
+  "CMakeFiles/POO.dir/src/Tiempo.cpp.o.d"
+  "CMakeFiles/POO.dir/src/main.cpp.o"
+  "CMakeFiles/POO.dir/src/main.cpp.o.d"
+  "POO"
+  "POO.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
